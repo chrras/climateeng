0fef439aa9a1f50a8270e2664e9269876ef71625
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2016-05-23 17:00:37 +0200

    Add test

--- tests/testthat/test_psychrometrics.R
+++ tests/testthat/test_psychrometrics.R
@@ -35,3 +35,27 @@ test_that("Humidity ratio validation", {
   expect_identical(round(hum_ratio(40, 27, alt = 0), 3), 0.009)
   expect_identical(round(hum_ratio(10, 4, alt = 0), 4), 0.0005)
 })
+
+test_that("Moist air density validation", {
+  expect_identical(round((1 + 0.0195) / density_air(35, 0.0195), 2), 0.90)
+  expect_identical(round((1 + 0.0075) / density_air(13, 0.0075), 2), 0.82)
+  expect_identical(round((1 + 0.0090) / density_air(47, 0.0090), 2), 0.92)
+})
+
+test_that("Enthalpy intersect validation", {
+  expect_equal(enthalpy_intersect(30, alt = 0), 10.3)
+  expect_equal(enthalpy_intersect(20, alt = 0), 5.7)
+  expect_equal(enthalpy_intersect(69, alt = 0), 23.2)
+})
+
+test_that("Enthalpy validation", {
+  expect_equal(round(enthalpy(22, 0.011), 0), 50)
+  expect_equal(round(enthalpy(46, 0.0285), 0), 120)
+  expect_equal(round(enthalpy(5, 0.002), 0), 10)
+})
+
+test_that("Humidity ratio by enthalpy validation", {
+  expect_equal(round(hum_ratio_enthalpy(50, 22, alt = 0), 3), 0.011)
+  expect_equal(round(hum_ratio_enthalpy(120, 46, alt = 0), 4), 0.0285)
+  expect_equal(round(hum_ratio_enthalpy(10, 5, alt = 0), 3), 0.002)
+})
