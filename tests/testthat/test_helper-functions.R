cec1e0d8524d9d67ab71d54ed723e5df5d23deb9
Christoffer Rasmussen <christofferrasmussen.1987@gmail.com>
2016-05-23 15:46:14 +0200

    Update test names

--- tests/testthat/test_helper-functions.R
+++ tests/testthat/test_helper-functions.R
@@ -1,7 +1,7 @@
 library(climateeng)
 context("Helper functions")
 
-test_that("Floor and ceiling in my-round works", {
+test_that("Check my_round()", {
   expect_identical(my_round(c(24.9, 20.1, 25), 5, "ceiling"), c(25, 25, 25))
   expect_identical(my_round(24.9, 5, "floor"), 20)
 })
--- tests/testthat/test_psychrometrics.R
+++ tests/testthat/test_psychrometrics.R
@@ -1,12 +1,12 @@
 library(climateeng)
 context("Psychrometrics")
 
-test_that("Barometric pressure equals table values in ASHRAE handbook", {
+test_that("Barometric pressure match table values in ASHRAE handbook", {
   expect_equal(bar_press(), 101.325)
   expect_equal(round(bar_press(alt = 10000), 3), 26.436)
 })
 
-test_that("Dew-point is within tolerance", {
+test_that("Dew-point tolerance", {
   expect_equal(dewpoint(hum.ratio = 0), NA)
   expect_lt(
     max(na.omit(abs(par_w_press(seq(0, 0.050, by = 0.001), 0) /
@@ -14,7 +14,7 @@ test_that("Dew-point is within tolerance", {
     0.25)
 })
 
-test_that("Relative humidity does not exceed 100 %", {
+test_that("Check 100 % relative humidity limit", {
   hum.ratio <- 0.010
   temp.air <- 10
   alt <- 0
@@ -23,14 +23,14 @@ test_that("Relative humidity does not exceed 100 %", {
   expect_gt(par_w_press(hum.ratio, alt) / sat_w_press(temp.air) * 100, 100)
 })
 
-test_that("Relative humidity fits ASHRAE Handbook psychrometric chart", {
+test_that("Check if relative humidity fits ASHRAE Handbook psychrometric chart", {
   expect_identical(round(rel_hum(40, 0.00925, alt = 0), 0), 20)
   expect_identical(round(rel_hum(23, 0.016, alt = 0), 0), 90)
   expect_identical(round(rel_hum(0, 0.003, alt = 0), 0), 80)
   expect_identical(round(rel_hum(4, 0.0005, alt = 0), 0), 10)
 })
 
-test_that("Humidity ratio fits ASHRAE Handbook psychrometric chart", {
+test_that("Check if humidity ratio fits ASHRAE Handbook psychrometric chart", {
   expect_identical(round(hum_ratio(100, 24, alt = 0), 3), 0.019)
   expect_identical(round(hum_ratio(40, 27, alt = 0), 3), 0.009)
   expect_identical(round(hum_ratio(10, 4, alt = 0), 4), 0.0005)
